75ab307e066543eca0ea9b6b781826bf686bef20
Erika-Michel <erikamichel174@gmail.com>
2021-05-21 16:12:24 +0200

    Added Report.md

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int balance = 2_147_483_000;
+        int transfer = 647;
+        int total = balance + transfer;
+        System.out.println(total);
+    }
+}
